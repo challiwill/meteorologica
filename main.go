f753ff55c647ae33d52a2b1a782cb3a6e1ba081e
Corbin Halliwill <challiwill@pivotal.io>
2016-09-19 16:29:02 -0700

    run at midnight

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/challiwill/meteorologica/datamodels"
 	"github.com/challiwill/meteorologica/gcp"
 	"github.com/gocarina/gocsv"
+	"github.com/robfig/cron"
 )
 
 type Client interface{}
@@ -37,86 +38,86 @@ func main() {
 	log := logrus.New()
 	log.Out = os.Stderr
 
-	// HEALTHCHECK
-	go func() {
-		http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Meteorologica is deployed.")
-		})
-		log.Fatal(http.ListenAndServe(":8080", nil))
-	}()
+	runTime := time.Time{}
 
 	// BILLING DATA
-	go func() {
-		t := time.NewTicker(24 * time.Hour)
-		for tick := range t.C {
-			log.Info("Running periodic job at %s ...\n", tick.String())
-			normalizedFileName := strings.Join([]string{
-				strconv.Itoa(time.Now().Year()),
-				time.Now().Month().String(),
-				strconv.Itoa(time.Now().Day()),
-				"normalized-billing-data.csv",
-			}, "-")
-			normalizedFile, err := os.Create(normalizedFileName)
-			if err != nil {
-				log.Fatal("Failed to create normalized file: ", err.Error())
-			}
-
-			// AZURE CLIENT
-			if getAzure || getAll {
-				normalizedAzure, err := getAzureUsage(log)
-				if err != nil {
-					log.Error("Failed to get Azure usage data: ", err.Error())
-				} else {
-					err = gocsv.MarshalFile(&normalizedAzure, normalizedFile)
-					if err != nil {
-						log.Error("Failed to write normalized Azure data to file: ", err.Error())
-					} else {
-						log.Info("Wrote normalized Azure data to ", normalizedFile.Name())
-					}
-				}
-			}
-
-			// GCP CLIENT
-			if getGCP || getAll {
-				normalizedGCP, err := getGCPUsage(log)
-				if err != nil {
-					log.Error("Failed to get GCP usage data: ", err.Error())
-				} else {
-					err = gocsv.MarshalFile(&normalizedGCP, normalizedFile)
-					if err != nil {
-						log.Error("Failed to write normalized GCP data to file: ", err.Error())
-					} else {
-						log.Info("Wrote normalized GCP data to ", normalizedFile.Name())
-					}
-				}
-			}
-
-			// AWS CLIENT
-			if getAWS || getAll {
-				normalizedAWS, err := getAWSUsage(log)
-				if err != nil {
-					log.Error("Failed to get AWS usage data: ", err.Error())
-				} else {
-					err = gocsv.MarshalFile(&normalizedAWS, normalizedFile)
-					if err != nil {
-						log.Error("Failed to write normalized AWS data to file: ", err.Error())
-					} else {
-						log.Info("Wrote normalized AWS data to ", normalizedFile.Name())
-					}
-				}
-			}
-			// Publish to AWS
-			log.Info("Finished periodic job at %s.\n", time.Now().String())
+	c := cron.New()
+	c.AddFunc("@midnight", func() {
+		log.Infof("Running periodic job at %s ...", time.Now().String())
+		runTime = time.Now()
+		normalizedFileName := strings.Join([]string{
+			strconv.Itoa(time.Now().Year()),
+			time.Now().Month().String(),
+			strconv.Itoa(time.Now().Day()),
+			"normalized-billing-data.csv",
+		}, "-")
+		normalizedFile, err := os.Create(normalizedFileName)
+		if err != nil {
+			log.Fatal("Failed to create normalized file: ", err.Error())
 		}
-	}()
 
+		// AZURE CLIENT
+		if getAzure || getAll {
+			normalizedAzure, err := getAzureUsage(log)
+			if err != nil {
+				log.Error("Failed to get Azure usage data: ", err.Error())
+			} else {
+				err = gocsv.MarshalFile(&normalizedAzure, normalizedFile)
+				if err != nil {
+					log.Error("Failed to write normalized Azure data to file: ", err.Error())
+				} else {
+					log.Info("Wrote normalized Azure data to ", normalizedFile.Name())
+				}
+			}
+		}
+
+		// GCP CLIENT
+		if getGCP || getAll {
+			normalizedGCP, err := getGCPUsage(log)
+			if err != nil {
+				log.Error("Failed to get GCP usage data: ", err.Error())
+			} else {
+				err = gocsv.MarshalFile(&normalizedGCP, normalizedFile)
+				if err != nil {
+					log.Error("Failed to write normalized GCP data to file: ", err.Error())
+				} else {
+					log.Info("Wrote normalized GCP data to ", normalizedFile.Name())
+				}
+			}
+		}
+
+		// AWS CLIENT
+		if getAWS || getAll {
+			normalizedAWS, err := getAWSUsage(log)
+			if err != nil {
+				log.Error("Failed to get AWS usage data: ", err.Error())
+			} else {
+				err = gocsv.MarshalFile(&normalizedAWS, normalizedFile)
+				if err != nil {
+					log.Error("Failed to write normalized AWS data to file: ", err.Error())
+				} else {
+					log.Info("Wrote normalized AWS data to ", normalizedFile.Name())
+				}
+			}
+		}
+		// Publish to AWS
+		log.Infof("Finished periodic job at %s.", time.Now().String())
+	})
+
+	c.Start()
+
+	// HEALTHCHECK
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Meteorologica is deployed, last job ran at: %s", runTime.String())
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	os.Exit(0)
 }
 
 func getAzureUsage(log *logrus.Logger) (datamodels.Reports, error) {
 	azureClient := azure.NewClient("https://ea.azure.com/", os.Getenv("AZURE_ACCESS_KEY"), os.Getenv("AZURE_ENROLLMENT_NUMBER"))
 
-	log.Debug("Getting Monthly Azure Usage...")
+	log.Info("Getting Monthly Azure Usage...")
 	azureMonthlyusage, err := azureClient.MonthlyUsageReport()
 	if err != nil {
 		log.Error("Failed to get Azure monthly usage")
@@ -158,7 +159,7 @@ func getGCPUsage(log *logrus.Logger) (datamodels.Reports, error) {
 		return datamodels.Reports{}, err
 	}
 
-	log.Debug("Getting Monthly GCP Usage...")
+	log.Info("Getting Monthly GCP Usage...")
 	gcpMonthlyUsage, err := gcpClient.MonthlyUsageReport()
 	if err != nil {
 		log.Error("Failed to get GCP monthly usage")
@@ -209,7 +210,7 @@ func getAWSUsage(log *logrus.Logger) (datamodels.Reports, error) {
 
 	awsClient := aws.NewClient(os.Getenv("AWS_BUCKET_NAME"), os.Getenv("AWS_MASTER_ACCOUNT_NUMBER"), sess)
 
-	log.Debug("Getting Monthly AWS Usage...")
+	log.Info("Getting Monthly AWS Usage...")
 	awsMonthlyUsage, err := awsClient.MonthlyUsageReport()
 	if err != nil {
 		log.Error("Failed to get AWS monthly usage: ", err)
