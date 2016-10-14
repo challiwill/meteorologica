145527f18c6e84615aace80f19545e02733ec6d1
Corbin Halliwill <challiwill@pivotal.io>
2016-10-14 11:14:06 -0700

    use min/max properly

--- gcp/client.go
+++ gcp/client.go
@@ -72,7 +72,7 @@ func (c Client) GetNormalizedUsage() (datamodels.Reports, error) {
 	monthlyReport := []*Usage{}
 	for i, usage := range gcpMonthlyUsage {
 		var readerCleaner *csv.ReaderCleaner
-		readerCleaner, err = csv.NewReaderCleaner(bytes.NewReader(usage), 14, 18) // ambiguously 18 and 14...
+		readerCleaner, err = csv.NewReaderCleaner(bytes.NewReader(usage), 18, 14) // ambiguously 18 and 14...
 		if err != nil {
 			return datamodels.Reports{}, err
 		}
