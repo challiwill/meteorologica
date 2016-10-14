317bf0649655c254279bb17f3469f35a5a5bcb1d
Corbin Halliwill <challiwill@pivotal.io>
2016-10-14 14:31:08 -0700

    clean up sql table description

--- migrations/iaas_billing.sql
+++ migrations/iaas_billing.sql
@@ -1,1 +1,15 @@
-CREATE TABLE cf_82db49c0_7f6f_4844_a8af_97788db5e92e.iaas_billing ( AccountNumber VARCHAR(15) NOT NULL, AccountName VARCHAR(30), Day TINYINT(2) NOT NULL, Month CHAR(9) NOT NULL, Year SMALLINT(4) NOT NULL, ServiceType VARCHAR(30) NOT NULL, UsageQuantity DOUBLE NOT NULL, Cost DECIMAL(15,2) NOT NULL, Region VARCHAR(10), UnitOfMeasure VARCHAR(10), IAAS VARCHAR(10) NOT NULL, UNIQUE KEY(AccountNumber, Day, Month, Year, ServiceType, Region, IAAS));
+CREATE TABLE cf_82db49c0_7f6f_4844_a8af_97788db5e92e.iaas_billing
+(
+ AccountNumber VARCHAR(15) NOT NULL, 
+AccountName VARCHAR(30),
+Day TINYINT(2) NOT NULL,
+Month CHAR(9) NOT NULL,
+Year SMALLINT(4) NOT NULL,
+ServiceType VARCHAR(30) NOT NULL,
+UsageQuantity DOUBLE NOT NULL,
+Cost DECIMAL(15,2) NOT NULL,
+Region VARCHAR(10),
+UnitOfMeasure VARCHAR(10),
+IAAS VARCHAR(10) NOT NULL,
+UNIQUE KEY(AccountNumber, Day, Month, Year, ServiceType, Region, IAAS)
+);
